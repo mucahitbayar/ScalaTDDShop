037d6885f18a66896121275d2910d55aa4f769bf
Mucahit Bayar <mucahitbayar@gmail.com>
2017-06-23 13:52:05 +0100

    add logic to calculate total price

--- src/main/scala/com/mucahitbayar/shoppingcart/CheckOut.scala
+++ src/main/scala/com/mucahitbayar/shoppingcart/CheckOut.scala
@@ -5,7 +5,25 @@ import com.mucahitbayar.shoppingcart.domain.Product
 object CheckOut {
   val products:Set[Product]=Set(Product("Apple", 0.60),Product("Orange", 0.25))
   def main(args: Array[String]): Unit = {
-
+    val items: List[Product] = getProducts(args)
+    val formattedPrice: String = formatPrice(items)
+    val totalString: String = getTotalToDisplay(items, formattedPrice)
+    println(totalString)
   }
 
+   def getTotalToDisplay(items: List[Product], formattedPrice: String) = {
+    val totalString = s"[ ${items.map(p => p.name).mkString(", ")} ] => ${formattedPrice}"
+    totalString
+  }
+
+  def getProducts(args: Array[String]) = {
+    val items = args.flatMap(s => products.find(p => p.isEqual(s))).toList
+    items
+  }
+
+   def formatPrice(items: List[Product]) = {
+    val totalPrice = items.map(p => p.price).sum
+    val totalString = if (totalPrice > 1) s"£${totalPrice}" else s"${totalPrice * 100}p"
+    totalString
+  }
 }
--- src/test/scala/com/mucahitbayar/shoppingcart/CheckOutTest.scala
+++ src/test/scala/com/mucahitbayar/shoppingcart/CheckOutTest.scala
@@ -3,20 +3,36 @@ package com.mucahitbayar.shoppingcart
 import com.mucahitbayar.shoppingcart.domain.Product
 import org.scalatest.FlatSpec
 
-class CheckOutTest extends FlatSpec  {
+class CheckOutTest extends FlatSpec {
 
-  val apple =Product("Apple", 0.60)
+  val apple = Product("Apple", 0.60)
   val orange = Product("Orange", 0.25)
 
 
   behavior of "products"
 
   it should "have only 1 apple" in {
-    CheckOut.products.count(p=>p.isEqual(apple)) ===1
+    CheckOut.products.count(p => p.isEqual(apple)) === 1
   }
 
   it should "have only 1 orange" in {
-    CheckOut.products.count(p=>p.isEqual(orange)) === 1
+    CheckOut.products.count(p => p.isEqual(orange)) === 1
+  }
+
+
+  behavior of "getProducts"
+
+  it should "return existing products" in {
+    val args = Array("Apple", "Orange")
+    val items = CheckOut.getProducts(args)
+    items.forall(p => CheckOut.products.exists(c => c.isEqual(p)))
+  }
+
+  "getTotalToDisplay" should "print products" in {
+    val args = Array("Apple", "Orange")
+    val expected = "[ Apple, Apple, Orange, Apple ] => £2.05"
+    val actual = CheckOut.getProducts(args)
+    expected === actual
   }
 
 }
